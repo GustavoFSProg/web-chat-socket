b84f4a46b097132d6b61fff27737c1053e49138a
Gustavo <gustavo.prog40@gmail.com>
2023-04-19 18:30:08 -0300

    novamente

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -10,7 +10,7 @@ export default function Join({setChatVisibility, setSocket}) {
   const handleSubmit = async () => {
     const username = usernameRef.current.value
     if(!username.trim()) return
-    const socket = await io.connect('http://localhost:3001')
+    const socket = await io.connect('https://chat-socket-server-api.vercel.app/')
     socket.emit('set_username', username)
     setSocket(socket)
     setChatVisibility(true)
